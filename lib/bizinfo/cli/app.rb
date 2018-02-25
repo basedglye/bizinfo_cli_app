1822db14ef9a4de3eb6fb0f3f6ab18d8b3a95b7c
Frank McCallion <keith@sensortech.com>
2018-02-25 18:42:35 +0000

    ran bundler

---
+++ lib/bizinfo/cli/app.rb
@@ -0,0 +1,9 @@
+require "bizinfo/cli/app/version"
+
+module Bizinfo
+  module Cli
+    module App
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bizinfo/cli/app/version.rb
@@ -0,0 +1,7 @@
+module Bizinfo
+  module Cli
+    module App
+      VERSION = "0.1.0"
+    end
+  end
+end
